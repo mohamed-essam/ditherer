ee4bb93f01e699321a0c4b52517e78e7031ef157
Mohamed Essam <metopboy1998@gmail.com>
2016-09-15 16:28:13 +0200

    Color pallete

--- dither/ditherer.py
+++ dither/ditherer.py
@@ -1,6 +1,8 @@
 from PIL import Image
 
-color_pallete = [(0,0,0), (255,0,0), (0,255,0), (0,0,255), (255,255,255)]
+BLEEDING_THRESHOLD = 0.9
+
+color_pallete = [(0,0,0), (255,255,255), (128,0 ,0), (255, 0, 0), (0, 128, 0), (0, 255, 0), (0, 0, 128), (0, 0, 255)]
 
 def adjust_error(color, error):
     return (color[0] + error[0], color[1] + error[1], color[2] + error[2])
@@ -23,12 +25,12 @@ def _get_color(color):
             error = color_error(color, cp)
     return closest_color, error
 
-def add_error(forward_array, index, error, size):
+def add_error(forward_array, index, error, size, forward_index):
     i, j = index
     w, h = size
     if i < 0 or i >= h or j < 0 or j >= w:
         return
-    forward_array[i*w+j] += error
+    forward_array[forward_index*w+j] += error
 
 def get_algorithm_data(algorithm):
     if algorithm == "sierra3":
@@ -78,26 +80,31 @@ def get_algorithm_data(algorithm):
     return error_dist, error_offset, error_divisor
 
 
-def distribute_error(forward_array, index,error, size, algorithm):
+def distribute_error(forward_array, index,error, size, algorithm, forward_index):
     i, j = index
     w, h = size
     error_dist, error_offset, error_divisor = get_algorithm_data(algorithm)
     for k in range(len(error_dist)):
         for l in range(-error_offset, error_offset+1):
-            add_error(forward_array, (i+k, j + l), error * error_dist[k][l+error_offset] / error_divisor, size)
+            add_error(forward_array, (i+k, j + l), error * error_dist[k][l+error_offset] / error_divisor, size, (forward_index+k)%3)
 
 def dither_image(image, algorithm = "sierra3"):
     width, height = image.size
     pixels = image.getdata()
     new_pixels = []
-    forward_array_r = [0.0 for _ in range(width*height)]
-    forward_array_g = [0.0 for _ in range(width*height)]
-    forward_array_b = [0.0 for _ in range(width*height)]
+    forward_array_r = [0.0 for _ in range(3*width)]
+    forward_array_g = [0.0 for _ in range(3*width)]
+    forward_array_b = [0.0 for _ in range(3*width)]
+    forward_index = 0
     for i in range (height):
         for j in range(width):
-            new_color, error = _get_color(adjust_error(pixels[i*width+j], (forward_array_r[i*width+j], forward_array_g[i*width+j], forward_array_b[i*width+j])))
+            new_color, error = _get_color(adjust_error(pixels[i*width+j], (forward_array_r[forward_index*width+j], forward_array_g[forward_index*width+j], forward_array_b[forward_index*width+j])))
             new_pixels.append(new_color)
-            distribute_error(forward_array_r, (i,j), error[0], image.size, algorithm)
-            distribute_error(forward_array_g, (i,j), error[1], image.size, algorithm)
-            distribute_error(forward_array_b, (i,j), error[2], image.size, algorithm)
+            distribute_error(forward_array_r, (i,j), error[0] * BLEEDING_THRESHOLD, image.size, algorithm, forward_index)
+            distribute_error(forward_array_g, (i,j), error[1] * BLEEDING_THRESHOLD, image.size, algorithm, forward_index)
+            distribute_error(forward_array_b, (i,j), error[2] * BLEEDING_THRESHOLD, image.size, algorithm, forward_index)
+        for j in range(width):
+            forward_array_r[forward_index*width+j] = forward_array_g[forward_index*width+j] = forward_array_b[forward_index*width+j] = 0.0
+        forward_index+=1
+        forward_index %= 3
     image.putdata(new_pixels)
